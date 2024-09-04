a46bbcb05aecafced4b6517d7dbc97f48d670c38
Rodrigo Cesar <rodrigocnn@gmail.com>
2024-09-04 14:32:53 -0300

    feat: add protection to routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   layout :custom_layout
+  before_action :authenticate_admin!
 
   def index
     @instructors = Instructor.all
@@ -12,6 +13,14 @@ class ApplicationController < ActionController::Base
       'admin'
     else
       'application'
+    end
+  end
+  
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(Admin)
+      dashboard_path
+    else
+      root_path # ou qualquer outra rota para usuários regulares
     end
   end
 end
